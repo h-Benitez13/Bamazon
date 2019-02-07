46799a4dde8bf0ac037c78874ae705671b97c742
h-Benitez13 <hbenitez3711@gmail.com>
2019-02-06 21:28:14 -0500

    inquirer. Bug for checking quantity

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,5 @@
 var mysql = require("mysql");
-// var inquirer = require("inquirer");
+var inquirer = require("inquirer");
 
 // create the connection information for the sql database
 var connection = mysql.createConnection({
@@ -23,17 +23,99 @@ connection.connect(function (err) {
     start();
 });
 
-function start(){
-    connection.query("SELECT * FROM products", function(err, res) {
+function start() {
+    connection.query("SELECT * FROM products", function (err, res) {
         if (err) throw err;
         console.log("\n---------- Bamazon Products ---------\n");
-        for(var i=0; i< res.length; i++){
-        console.log(res[i].item_id + " | " + res[i].product_name + " | " + "$" + res[i].price + "\n");
-        
+        for (var i = 0; i < res.length; i++) {
+            console.log(res[i].item_id + " | " + res[i].product_name + " | " + "$" + res[i].price + "\n");
+
 
         }
-        connection.end();
         
-      });
+        welcomeID();
+
+    });
 }
 
+// function which prompts the user for what action they should take
+function welcomeID() {
+    inquirer
+        .prompt(
+            {
+                name: "welcome",
+                type: "input",
+                message: "Welcome to Bamazon! What's your name shopper?"
+            }
+        )
+        .then(function (answer) {
+            console.log(answer.welcome + ", " + "I hope you are ready to shop till you drop!\n");
+            idQuantity();
+
+        });
+}
+
+function idQuantity() {
+    // query the database for all items being purchased
+    connection.query("SELECT * FROM products", function (err, results) {
+        if (err) throw err;
+        // once you have the items, prompt the user for which they'd like to buy
+        inquirer
+            .prompt([
+                {
+                    name: "choice",
+                    type: "rawlist",
+                    choices: function () {
+                        var choiceArray = [];
+                        for (var i = 0; i < results.length; i++) {
+                            choiceArray.push(results[i].product_name);
+                        }
+                        return choiceArray;
+                    },
+                    message: "What item would you like to buy?"
+                },
+                {
+                    name: "quantity",
+                    type: "input",
+                    message: "How much would you like to buy?(in terms of quantity)"
+                }
+            ])
+            .then(function (answer) {
+                // get the information of the chosen item
+                for (var i = 0; i < results.length; i++) {
+                    if (results[i].product_name === answer.choice) {
+                        chosenItem = results[i];
+                    }
+                }
+                var chosenItem;
+                console.log("\n You want to buy " + answer.choice + 
+                    "\n'Cause you mad stingy, you want " + answer.quantity + answer.choice + "s");
+                 // determine if quantity is too high and over stock quantity
+        if (chosenItem.product_quantity <= parseInt(answer.quantity)) {
+            // quantity was low enough, so update db, let the user know, and start over
+            connection.query(
+              "UPDATE products SET ? WHERE ?",
+              [
+                {
+                  product_quantity: answer.quantity
+                },
+                {
+                  id: chosenItem.item_id
+                }
+              ],
+              function(error) {
+                if (error) throw err;
+                console.log("Sorry my guy, pero like, we aint got it like that. Try again...");
+                start();
+              }
+            );
+          }
+          else {
+            // quantity was TOO much, so apologize and start over
+            console.log("YOU ARE IN LUCK, WE HAVE JUST THE RIGHT AMOUNT, but keep shopping tho");
+            start();
+          }
+        });
+       
+    });
+  }
